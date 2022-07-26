e1f8985957459c08a3cc038620a6f8480e89f4e0
Bilal <bhaidar@gmail.com>
2022-07-27 02:15:33 +0300

    Preventing duplicate Payment Intents

--- app/Http/Controllers/PaymentIndexController.php
+++ app/Http/Controllers/PaymentIndexController.php
@@ -9,14 +9,20 @@ class PaymentIndexController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $paymentIntent = app('stripe')->paymentIntents->create([
-            'amount' => 10000, // cents
-            'currency' => 'usd',
-            'setup_future_usage' => 'on_session', // https://stripe.com/docs/api/payment_intents/create#create_payment_intent-setup_future_usage
-            'metadata' => [
-                'user_id' => (string) $request->user()->id,
-            ],
-        ]);
+        if (! session()->has('payment_intent_id')) {
+            $paymentIntent = app('stripe')->paymentIntents->create([
+                'amount' => 10000, // cents
+                'currency' => 'usd',
+                'setup_future_usage' => 'on_session', // https://stripe.com/docs/api/payment_intents/create#create_payment_intent-setup_future_usage
+                'metadata' => [
+                    'user_id' => (string) $request->user()->id,
+                ],
+            ]);
+
+            session()->put('payment_intent_id', $paymentIntent->id);
+        } else {
+            $paymentIntent = app('stripe')->paymentIntents->retrieve(session()->get('payment_intent_id'));
+        }
 
         return Inertia::render('Payments/Index', ['paymentIntent' => $paymentIntent]);
     }
