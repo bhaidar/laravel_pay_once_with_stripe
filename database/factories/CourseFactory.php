9c49351494cbbe582329087db05a48d1c25b3eec
Bilal <bhaidar@gmail.com>
2022-07-27 19:31:07 +0300

    Add course model with seeder

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(20),
+            'starts_at' => $this->faker->dateTimeThisYear('+2 weeks'),
+        ];
+    }
+}
---
+++ database/seeders/CourseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Course::factory()
+            ->count(5)
+            ->create();
+    }
+}
