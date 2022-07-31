40c39ef32d0a7c05e15eca1dfe3487cc34a0d67a
Bilal <bhaidar@gmail.com>
2022-07-31 23:13:20 +0300

    Transfer the cart to the authenticated user, when products can be publically added to cart

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\Cart;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -33,9 +34,21 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request)
     {
+        $cart = Cart::bySession()->first();
+
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        // If Cart record exists for the same user who is trying to login
+        // Bring it over, update it's session_id with the newly generated one
+        // This way, we link anonymous user (who have added products to cart before signing in)
+        // with the new session after signing in.
+        // Also update the user_id field to reflect the signed in user
+        $cart?->update([
+            'session_id' => session()->getId(),
+            'user_id' => auth()->id(),
+        ]);
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
@@ -48,11 +61,19 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request)
     {
+        $cart = Cart::bySession()->first();
+
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
+
+        // After logging out, the user maintains the cart items
+        // However, we need to update the session_id to reflect the newly created one
+        $cart?->update([
+            'session_id' => session()->getId(),
+        ]);
 
         return redirect('/');
     }
