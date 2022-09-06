4c131094ee0e6b4f5bb332ca18859f606e30ec98
Bilal <bhaidar@gmail.com>
2022-09-06 20:55:57 +0300

    Db listen with stack trace

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,12 +26,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // https://medium.com/@cosmeescobedo/how-to-find-the-slowest-query-in-your-laravel-application-76ba6de04716
         DB::listen(function ($query) {
-            \Log::info(
-                $query->sql,
-                $query->bindings,
-                $query->time
-            );
+            // grab the first element of non vendor/ calls
+            $location = collect(debug_backtrace())->filter(function ($trace) {
+                return ! str_contains($trace['file'], 'vendor/');
+            })->first();
+
+            $bindings = implode(', ', $query->bindings); // format the bindings as string
+
+            \Log::info("
+                — — — — — —
+                Sql: $query->sql
+                Bindings: $bindings
+                Time: $query->time
+                File: ${location['file']}
+                Line: ${location['line']}
+                — — — — — —
+            ");
         });
 
         // To prevent wrapping results of Resource Collection with the 'data' field
